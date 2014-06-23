9f36abab602f27e982e9edece0c8e278590b18a0
Brian Collins <bcollins@newrelic.com>
2014-06-23 13:48:18 -0700

    more changes for production

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
 (function() {
 
-  var salesPriorityID = 24001886;   // sandbox
-  //var salesPriorityID = 0;          // prodction
+// testing on https://newrelic.zendesk.com/agent/#/tickets/88903
+// known issues:
+// some older tickets will not pull the tags from the old drivers
+
+  // Sales Priority
+  var salesPriorityID = 20883706;          // testing in prodction
   var salesPriorityTags = ["sp0", "sp1", "sp2", "sp3", "sp4", "sp5", "sp6", "sp7", "sp8"];
 
+  // New Product
+  var newProductID = 23854248;
+  var newProductTags = ["apm", "mobile", "insights", "browser", "platform", "servers", "else", "demo_request"];
 
+  // New Platform / else
+  var newPlatformElseID = 23854258;
+  var newPlatformElseTags = ["dotnet", "account_billing", "agent_sdk", "alerting", "android", "early_access", "feature_request", "ios", "java", "linux", "ios_app", "android_app", "site", "node_js", "other", "php", "python", "ruby", "solaris", "windows"];
 
-  return { // the entire app goes inside this return block!
-    // listen for API events such as the start of our app, when bits of it get clicked on or when AJAX requests complete
+  // New Driver
+  var newDriverID = 23854268;
+  var newDriverTags = ["install", "crasher", "data_issue", "api", "mgi", "interp", "cust_dash", "develop", "publish", "billing", "signin", "upgrade", "downgrade", "cancel", "promo", "swag", "alert_wsm", "alert_lsm", "alert_smos", "alert_app", "alert_availmon", "rum", "sub_other"];
+
+  return { 
+    
     events: {
-      'app.activated':                  'initialize',
       'click #populate_drivers_button': 'pull_tags'
     },
 
-    // This app doesn't make any AJAX requests but they would go here if it did
     requests: {
-
-    },
-
-    initialize: function(data) { // function called when we load
-      if (data.firstLoad) {
-
-      }
     },
     
     pull_tags: function() {
       console.log("pull_tags has been fired");
 
-      //example of how this could work in theory 
       var ticket = this.ticket();
-      var ticketTags = this.ticket().tags();
+      var ticketTags = ticket.tags();
 
       for (var i = 0; i < ticketTags.length; i++) {
-
         if (salesPriorityTags.indexOf(ticketTags[i]) > -1) {
           ticket.customField("custom_field_" + salesPriorityID, ticketTags[i]);
         }
-        // parse other possible tags into the correct fields... going to be quite a bit ot work
-      
+        if (newProductTags.indexOf(ticketTags[i]) > -1) {
+          ticket.customField("custom_field_" + newProductID, ticketTags[i]);
+        }  
+        if (newPlatformElseTags.indexOf(ticketTags[i]) > -1) {
+          ticket.customField("custom_field_" + newPlatformElseID, ticketTags[i]);
+        }  
+        if (newDriverTags.indexOf(ticketTags[i]) > -1) {
+          ticket.customField("custom_field_" + newDriverID, ticketTags[i]);
+        }  
       }
     }
   };
